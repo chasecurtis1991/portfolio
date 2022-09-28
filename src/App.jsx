5825c4bdb1cf38f85970d58c63109323d411a3c4
Chase Curtis <chasecurtis1991@gmail.com>
2022-09-27 21:24:35 -0400

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import Logo from "./assets/logo.svg";
 import "./App.css";
+import Navbar from "./Navbar"
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <img src={Logo} className="logo" alt="Chase Curtis logo" />
-      </div>
+      <header className="App-header">
+        <Navbar />
+      </header>
       <div className="card">
-        <p>New website coming soon!</p>
+        <ul className="text-white">
+          <li>About Me</li>
+          <li>Portfolio</li>
+          <li>Active Projects</li>
+          <li>Certifications</li>
+          <li>Contact</li>
+        </ul>
       </div>
     </div>
   );
